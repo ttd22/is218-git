5d3b6583aa36e5b6c48fee10729291ea3646423b
ttd22 <ttd22@njit.edu>
2022-02-03 21:12:49 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Trang Dang"
         return render_template('index.html', name=name)
